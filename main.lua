1ffde1301e069de9dba4c320021f8852eb84150b
xinsert <yknotkids@gmail.com>
2026-05-31 19:50:05 -0700

    Add drag functionality to top bar
    Implement draggable functionality for the top bar.

--- main.lua
+++ main.lua
@@ -249,3 +249,37 @@ button.MouseButton1Click:Connect(function()
 	main.Visible = isOpen
 	blur.Size = isOpen and 10 or 0
 end)
+
+--DRAGGABILITY
+local UIS = game:GetService("UserInputService")
+
+local dragging = false
+local dragStart
+local startPos
+
+topbar.InputBegan:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		dragging = true
+		dragStart = input.Position
+		startPos = main.Position
+	end
+end)
+
+topbar.InputEnded:Connect(function(input)
+	if input.UserInputType == Enum.UserInputType.MouseButton1 then
+		dragging = false
+	end
+end)
+
+UIS.InputChanged:Connect(function(input)
+	if dragging and input.UserInputType == Enum.UserInputType.MouseMovement then
+		local delta = input.Position - dragStart
+
+		main.Position = UDim2.new(
+			startPos.X.Scale,
+			startPos.X.Offset + delta.X,
+			startPos.Y.Scale,
+			startPos.Y.Offset + delta.Y
+		)
+	end
+end)
